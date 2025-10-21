da793b058fa296cd3a88122f60a99657d857d390
Oleh.Murenkyi <oleh.murenkyi@ndax.io>
2025-10-21 17:30:07 -0600

    added console

--- frontend/components/courses/all-courses.tsx
+++ frontend/components/courses/all-courses.tsx
@@ -27,6 +27,7 @@ export const AllCourses: React.FC = async () => {
     );
     const cardImage = card?.image ? `${getImageURL(card.image.url)}` : '';
     const postImage = item?.image?.url ? `${getImageURL(item.image.url)}` : '';
+    console.log({ cardImage, cardURL: card?.image?.url });
     return {
       id: item.id,
       title: item.title ?? card?.title ?? '',

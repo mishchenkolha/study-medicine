bfa98cbc36b8567e5e54ebb041c18a977724c828
olehm <mishchenkolha@gmail.com>
2026-02-13 18:50:13 -0700

    fixed build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -16,6 +16,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev();
--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { IS_PROD, SESSION_TIME } from './utils/constants';
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
   const requestHeaders = new Headers(request.headers);

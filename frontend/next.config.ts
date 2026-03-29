00cc9366b8ff32d7f00d795b8497366c5539ddee
mishchenkoolha <mishchenkolha@gmail.com>
2026-03-28 20:39:54 -0600

    fixed eval

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,14 +5,15 @@ import { RemotePattern } from 'next/dist/shared/lib/image-config';
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://challenges.cloudflare.com;
+    script-src 'self' 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline' https://challenges.cloudflare.com;
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: ${MEDIA_LIBRARY_URL} ${DOMAIN_URL};
+    img-src 'self' blob: data: https://strapi.medesthetexperts.com http://localhost:3000;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
     form-action 'self';
-    frame-src https://challenges.cloudflare.com;
+    frame-src 'self' https://challenges.cloudflare.com;
+    worker-src 'self' blob:;
     connect-src 'self' https://challenges.cloudflare.com https://api.resend.com;
     upgrade-insecure-requests;
 `
